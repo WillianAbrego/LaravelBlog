fa12bb1c67c80c34f4c28165103ff349ff4c4114
WillianAbrego <willianantonioabrego@gmail.com>
2024-01-21 16:11:42 -0600

    create relationships between models and methods

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $table = 'posts';
+    protected $fillable = ['id', 'title', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault('Admin User');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'post_tag');
+    }
 }
