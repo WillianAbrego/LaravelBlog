351db8da7ce8b3e93ec5b471f593793dc9d183bd
WillianAbrego <willianantonioabrego@gmail.com>
2024-01-24 17:29:03 -0600

    category routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +22,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' => 'auth', 'prefix' => 'dashboard'], function () {
+    //Dashboard
     Route::group(['prefix' => '', 'as' => 'dashboard'], function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('index');
+    });
+
+    //Categories
+    Route::group(['prefix' => 'categories', 'as' => 'categories.'], function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('index');
+        Route::get('create', [CategoryController::class, 'create'])->name('create');
+        Route::post('/', [CategoryController::class, 'store'])->name('store');
+        Route::get('{category:slug}/edit', [CategoryController::class, 'edit'])->name('edit');
+        Route::put('{category:slug}', [CategoryController::class, 'update'])->name('update');
+        Route::delete('{category:slug}/delete', [CategoryController::class, 'destroy'])->name('delete');
     });
 });
 
