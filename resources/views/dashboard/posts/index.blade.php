72b9d0caa75daadbfefa81be40cb7da56786b63a
WillianAbrego <willianantonioabrego@gmail.com>
2024-02-01 16:59:04 -0600

    livewire component post.index

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -24,7 +24,7 @@
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-        Livewre table
+        <livewire:post.index></livewire:post.index>
       </div>
     </div>
   </div>
