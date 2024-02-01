3792fd056f7fe74d758c6d5c5e9fa42ea7387805
WillianAbrego <willianantonioabrego@gmail.com>
2024-02-01 17:52:30 -0600

    view post.index

--- app/Livewire/Post/Index.php
+++ app/Livewire/Post/Index.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire\Post;
 
+use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+    public $search = '';
+    public $orderBy = 'id';
+    public $orderAsc = true;
+    public $perPage = 10;
+
+
     public function render()
     {
-        return view('livewire.posts.index');
+        return view('livewire.posts.index', [
+            'posts' => Post::searchPost($this->search)
+                ->orderBy($this->orderBy, $this->orderAsc ? 'asc' : 'desc')
+                ->paginate($this->perPage)
+        ]);
     }
 }
