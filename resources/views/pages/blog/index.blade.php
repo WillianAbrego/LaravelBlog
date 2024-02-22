949ede3188ea4436793064a5d8ee91aaf487ddd5
unknown <willianantonioabrego@gmail.com>
2024-02-22 15:05:24 -0600

    create livewire componet

--- resources/views/pages/blog/index.blade.php
+++ resources/views/pages/blog/index.blade.php
@@ -1,1 +1,13 @@
-this is a blog
+<x-base-layout>
+  <main class="min-h-screen">
+    <section class="container pt-24 mx-auto space-y-4">
+      {{-- Header --}}
+      <header class="w-full p-4 bg-white">
+        <h2>Header</h2>
+      </header>
+
+      {{-- Livewire ShowPost --}}
+      <livewire:post.show-post />
+    </section>
+  </main>
+</x-base-layout>
